060d7a7d68ed492be323123648ed5436d02287f3
David Bobb <david.bobb@txauus-ml-0003115.local>
2019-04-03 20:02:37 -0500

    rock paper scissors

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -10,7 +10,33 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
 
-  // Write code here
+if (hand1 == hand2) {
+  return "It's a tie!";
+};
+
+if (hand1 === "rock") {
+  if (hand2 === 'paper') {
+    return "Hand two wins!";
+  } else {
+    return "Hand one wins!";
+  };  
+};
+
+if (hand1 === "paper") {
+  if (hand2 === "rock") {
+    return "Hand one wins!";
+  } else {
+    return "Hand two wins!";
+  };
+};
+
+if (hand1 === "scissors") {
+  if (hand2 === "rock") {
+    return "Hand two wins!";
+  } else {
+    return "Hand one wins!";
+  };
+};
 
 }
 
