15821e8767da3c7e08c2ee60235a0b54804bb56c
David Bobb <david.bobb@txauus-ml-0003115.local>
2019-04-18 15:38:34 -0500

    style, y-logic, reset-logic

--- 03week/pigLatin-GUI/pigLatin.js
+++ 03week/pigLatin-GUI/pigLatin.js
@@ -1,12 +1,14 @@
 "use strict;"
 
+// This code does not handle punctuation
+// but can pig latin multiple words
+
 // get input value and convert to array (lowercase, trim)
 // code will then run through the pigLatin function for each array value
 // the it will convert the pig latin array into a string and return to output via DOM
 
 let getInputValue = function() {
   const pigLatinInput = document.querySelector("#input_pigLatin").value.toLowerCase().trim().split(" ");
-  console.log(pigLatinInput);
   let newPigLatinArray = [];
   for (let i = 0; i < pigLatinInput.length; i++) {
     newPigLatinArray.push(pigLatin(pigLatinInput[i]));
@@ -28,13 +30,24 @@ function pigLatin(word) {
       };
   
       const findVowelIndex = (vow) => {
-        return vow == "a" || vow == "e" || vow == "i" || vow == "o" || vow == "u";
+        return vow == "a" || vow == "e" || vow == "i" || vow == "o" || vow == "u" || vow == "y";
       }
-      let vowelIndex = val.findIndex(findVowelIndex);
-      let vowelEnd = val.splice(0, vowelIndex);
-      val.push(vowelEnd.join(""), 'a', 'y');
-      return val.join("");
+      if(val[0] == "y") {
+        val.shift();
+        val.push('y', 'a', 'y');
+        return val.join("");
+      } else {
+        let vowelIndex = val.findIndex(findVowelIndex);
+        let vowelEnd = val.splice(0, vowelIndex);
+        val.push(vowelEnd.join(""), 'a', 'y');
+        return val.join("");
+      }
     }
 
     return changeWords(trimmedWord);
+  }
+
+  const resetItems = function() {
+    document.querySelector("#input_pigLatin").value = "";
+    document.querySelector(".output").innerHTML = "";
   }
