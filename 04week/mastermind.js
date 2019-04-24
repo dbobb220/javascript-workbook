80fb4339628911ec6ca762ebc2632708636f7603
David Bobb <david.bobb@txauus-ml-0003115.attlocal.net>
2019-04-23 21:27:12 -0500

    additional logic and tests - 4 letters only, only a-h, test hints

--- 04week/mastermind.js
+++ 04week/mastermind.js
@@ -10,6 +10,7 @@ const rl = readline.createInterface({
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let colors = require('colors');
 
 function printBoard() {
   for (let i = 0; i < board.length; i++) {
@@ -30,8 +31,7 @@ function getRandomInt(min, max) {
 
 function generateHint(solution, guess) {
   let solutionArray = solution.split('');
-  let guessArray = guess.split('');
-  console.log(solutionArray, guessArray);
+  let guessArray = guess.toLowerCase().split('');
   let correctLetterLocations = 0;
   let correctLetters = 0;
   for (let i = 0; i < solutionArray.length; i++) {
@@ -45,23 +45,46 @@ function generateHint(solution, guess) {
     if (targetIndex > -1) {
       correctLetters += 1;
       solutionArray[j] = null;
-    };
+    }
   }
+  //! colors. is failing tests because it does not match expected output
+  // return colors.red(correctLetterLocations) + colors.green('-') + colors.white(correctLetters);
+  return correctLetterLocations + '-' + correctLetters;
 }
 
 function mastermind(guess) {
   solution = 'abcd'; // Comment this out to generate a random solution
-  if (guess == solution) {
+  let checkLetterSubmissions = guess.toLowerCase().split('');
+    // });
+  for (let z = 0; z < checkLetterSubmissions.length; z++) {
+    if (letters.indexOf(checkLetterSubmissions[z]) === -1) {
+      console.log('Only use letters a through h!');
+      return 'Only use letters a through h!';
+    }
+  }
+  //TODO: Simplify nested for loop
+  //? forEach doesn't respect return... How can I stop a forEach function when condition is met?
+  // checkLetterSubmissions.forEach(function(val) {
+  //   if (letters.indexOf(val) === -1) {
+  //     console.log('Only use letters a through h!');
+  //     return 'Only use letters a through h!';
+  //   }
+  if (guess.length !== 4) {
+    console.log('Use 4 letters only!');
+    return 'Use 4 letters only!';
+  } else if (guess == solution) {
     console.log('You guessed it!');
     return 'You guessed it!';
+  } else {
+    generateHint(solution, guess);
+    let hint = generateHint(solution, guess);
+    board.push(guess.toString() + ' ' + hint.toString());
   }
 }
-
 
 function getPrompt() {
   rl.question('guess: ', (guess) => {
     mastermind(guess);
-    generateHint(solution, guess);
     printBoard();
     getPrompt();
   });
@@ -79,16 +102,31 @@ if (typeof describe === 'function') {
     it('should be able to detect a win', () => {
       assert.equal(mastermind(solution), 'You guessed it!');
     });
+    it('should only allow 4 letters', () => {
+      assert.equal(mastermind('abcde'), 'Use 4 letters only!');
+    });
+    it('should only take letters a through h', () => {
+      assert.equal(mastermind('wxyz'), 'Only use letters a through h!');
+    });
   });
 
   describe('#generateHint()', () => {
     it('should generate hints', () => {
-      assert.equal(generateHint('abdc'), '2-2');
+      //! test does not pass because only 1 argument
+      // assert.equal(generateHint('abdc'), '2-2');
+      assert.equal(generateHint(solution, 'abdc'), '2-2');
     });
     it('should generate hints if solution has duplicates', () => {
-      assert.equal(generateHint('aabb'), '1-1');
+      //! test does not pass because only 1 argument
+      // assert.equal(generateHint('aabb'), '1-1');
+      assert.equal(generateHint(solution, 'aabb'), '1-1');
     });
-
+    it('should display the correct number of correctly located letters', () => {
+      assert.equal(generateHint(solution, 'abcf'), '3-0');
+    });
+    it('should display the number of matching letters that are not located corectly', () => {
+      assert.equal(generateHint(solution, 'badc'), '0-4');
+    });
   });
 
 } else {
