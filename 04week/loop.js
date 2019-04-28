ba3c2db06e3b8e3f3e7019a1d080faf5b5aa9b5a
dbobb220 <david.bobb.220@gmail.com>
2019-04-28 10:00:24 -0500

    finish last question

--- 04week/loop.js
+++ 04week/loop.js
@@ -27,10 +27,65 @@ const person = {
 // Use a for...in loop and if statement to console.log the value associated with the key birthDate if the birth year is an odd number.
 
 let birthYear = parseInt(person.birthDate.slice(7));
-console.log(typeof birthYear);
 
 for (let x in person) {
-    if (birthYear % 2 === 0) {
-        console.log(person.birthDate)
-    }
+    person[x] === person.birthDate ? console.log(person.birthDate.slice(-4))
+    : false;
 }
+
+// Create an arrayOfPersons that contains mulitiple objects. 
+// You can simply copy/paste the person object you made above multiple times. 
+// Feel free to change the values to reflect multiple people you might have in your database.
+
+let arrayOfPersons = [
+    {
+    firstName: "Jane",
+    lastName: "Doe",
+    birthDate: "Jan 5, 1925",
+    gender: "female"
+    },
+    {
+    firstName: "John",
+    lastName: "Doe",
+    birthDate: "Jul 3, 1983",
+    gender: "male"
+    },
+    {
+    firstName: "Jack",
+    lastName: "Smith",
+    birthDate: "Aug 10, 1957",
+    gender: "male"
+    },
+    {
+    firstName: "Emily",
+    lastName: "Knox",
+    birthDate: "Dec 25, 2000",
+    gender: "female"
+    }
+]
+
+// Use .map() to map over the arrayOfPersons and console.log() their information.
+
+arrayOfPersons.map(val => {
+    for (let x in val) {
+        console.log(val[x]);
+    }
+});
+
+// Use .filter() to filter the persons array and console.log only males in the array.
+
+arrayOfPersons.filter(val => {
+    for (let x in val) {
+        val[x] === "male" ? console.log(val) : false;
+    }
+});
+
+
+
+// Use .filter() to filter the persons array and console.log only people that were born before Jan 1, 1990.
+
+arrayOfPersons.filter(val => {
+    for (let x in val) {
+        val[x].slice(-4) < 1990 ? console.log(val) : false;
+    }
+});
